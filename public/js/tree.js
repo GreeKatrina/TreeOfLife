a72fcbcce401f611ea22f343fc7405c3d6e5abc7
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-06-28 14:09:25 -0500

    Commit before switching branches.

--- public/js/tree.js
+++ public/js/tree.js
@@ -54,6 +54,7 @@
           //set distance between node and its children
           levelDistance: 50,
           levelsToShow: 2,
+          constrained: false,
           //enable panning
           Navigation: {
             enable:true,
@@ -65,8 +66,11 @@
           Node: {
               height: 20,
               width: 150,
+              // type: 'nodeline',
               type: 'rectangle',
               color: '#fff',
+              // lineWidth: 2,
+              // align:"center",
               overridable: true
           },
           setColor: function(node){
@@ -91,16 +95,40 @@
           onAfterCompute: function(){
               Log.write("done");
           },
+          //Add a request method for requesting on-demand json trees.
+          //This method gets called when a node
+          //is clicked and its subtree has a smaller depth
+          //than the one specified by the levelsToShow parameter.
+          //In that case a subtree is requested and is added to the dataset.
+          //This method is asynchronous, so you can make an Ajax request for that
+          //subtree and then handle it to the onComplete callback.
+          //Here we just use a client-side tree generator (the getTree function).
+          request: function(nodeId, level, onComplete) {
+            $.ajax({
+              type: 'GET',
+              url: '/node-attributes',
+              data: { id: nodeId },
+              success: function(newNode) {
+                var ans = {
+                  'id': nodeId,
+                  'children': newNode.children
+                };
+                onComplete.onComplete(nodeId, ans);
+                console.log('oncompleteoncomplete')
+              },
+              onFailure: function(){
+                console.log("failed =(");
+              }
+            });
+          },
           //This method is called on DOM label creation.
           //Use this method to add event handlers and styles to
           //your node.
           onCreateLabel: function(label, node){
               label.id = node.id;
-              if (node.name){
-                label.innerHTML = node.name;
-              }
               var style = label.style;
-              if (node.name !== null) {
+              if (node.name) {
+                label.innerHTML = node.name;
                 style.width = 147 + 'px';
                 style.height = 17 + 'px';
                 style.cursor = 'pointer';
@@ -111,25 +139,36 @@
                 style.paddingTop = '3px';
               }
               label.onclick = function() {
-                  st.onClick(node.id, {
-                      onComplete: function() {
-                          $.ajax({
-                            type: 'GET',
-                            url: '/node-attributes',
-                            data: { id: node.id },
-                            success: function(newNode) {
-                              console.log(newNode.children);
-                              // for (var child in newNode.children){
-                              //   if (child.name === null){
-                              //     st.onClick(node.id);
-                              //   }
-                              // }
-                              st.addSubtree(newNode, 'replot');
-                            }
+                  if(!node.anySubnode("exist"))
+                     {
+                      node['collapsed']=true;
+                       node.eachSubgraph(function(subnode) {
+                         if(node.id!=subnode.id){
+                            subnode.drawn=false;
+                            subnode.setData('alpha',1);
+                         }
+                        });
+                        st.onClick(node.id);
+                     } else {
+                      st.onClick(node.id);
+                      node['collapsed']=false;
+                        node.eachSubgraph(function(subnode) {
+                           if(node.id!=subnode.id) {
+                              subnode.exist=false;
+                              subnode.drawn=false;
+                              subnode.setData('alpha',0);
+                           }
                           });
-                      }
-                  });
-              }
+                          st.move(node, {
+                            Move: {
+                              enable: true,
+                              offsetX:  0,
+                              offsetY:  0
+                            },
+                            onComplete: function() {}
+                          }); // move
+                        }
+                  };
           },
 
           //This method is called right before plotting
@@ -141,25 +180,38 @@
               //add some color to the nodes in the path between the
               //root node and the selected node.
               if (node.selected) {
-                  // set color of nodes along the selected path
-                  if(node.extinct){
-                      node.data.$color = "#490C60";
-                  }else{
-                      node.data.$color = "#8E44AD";
-                  }
+                // set color of nodes along the selected path
+                if (node.extinct) {
+                    node.data.$color = "#490C60";
+                } else {
+                    node.data.$color = "#8E44AD";
+                }
               } else {
-                  delete node.data.$color;
-                  //if the node belongs to the last plotted level
-                  if(!node.anySubnode("exist")) {
-                      if (node.extinct){
-                          node.data.$color = "#204ABA";
-                      }else{
-                          node.data.$color = '#317fd9';
-                      }
+                delete node.data.$color;
+                //if the node belongs to the last plotted level
+                // console.log("node: ", node);
+                // debugger;
+                // node.eachSubnode(function(n) {
+                //     st.removeSubtree(n.id, true, 'animate', {
+                //         hideLabels: false,
+                //         onComplete: function() {
+                //           removing = false;
+                //           console.log('subtree removed')
+                //           Log.write("subtree removed");
+                //         }
+                //     });
+                // });
+
+                if(!node.anySubnode("exist")) {
+                  if (node.extinct){
+                      node.data.$color = "#204ABA";
+                  } else {
+                      node.data.$color = '#317fd9';
                   }
+                }
               }
               // hacky way to make name-less nodes appear as lines
-              if (node.name === undefined) {
+              if (!node.name) {
                   if (node.selected) {
                       node.data.$height = 3;
                       node.data.$color = "#F2F1EF";
@@ -176,15 +228,15 @@
           //Edge data proprties prefixed with a dollar sign will
           //override the Edge global style properties.
           onBeforePlotLine: function(adj){
-              if (adj.nodeFrom.selected && adj.nodeTo.selected) {
-                  // Change color of lines in selected path
-                  adj.data.$color = "#F2F1EF";
-                  adj.data.$lineWidth = 3;
-              }
-              else {
-                  delete adj.data.$color;
-                  delete adj.data.$lineWidth;
-              }
+            if (adj.nodeFrom.selected && adj.nodeTo.selected) {
+                // Change color of lines in selected path
+                adj.data.$color = "#F2F1EF";
+                adj.data.$lineWidth = 3;
+            }
+            else {
+                delete adj.data.$color;
+                delete adj.data.$lineWidth;
+            }
           }
       });
       //load json data
