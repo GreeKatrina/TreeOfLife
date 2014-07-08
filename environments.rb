8f6673915522be9c9fa45bf96ab8d520308bfdce
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-07-08 13:07:57 -0500

    Edit environments file for heroku.

--- environments.rb
+++ environments.rb
@@ -3,14 +3,20 @@ require 'active_record_tasks'
 require 'sinatra'
 
 configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/TreeOfLife_development')
+  if ENV['DATABASE_URL']
+    ActiveRecord::Base.establish_connection(
+      :url => ENV['DATABASE_URL']
+    )
+  else
+    db = URI.parse( || 'postgres://localhost/TreeOfLife_development')
 
-  ActiveRecord::Base.establish_connection(
-    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
+    ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
+  end
 end
