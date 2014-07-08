160f70970e8b97c44c52f3c85872fd5dbd246362
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-07-08 12:28:34 -0500

    Edit environments.rb file.

--- environments.rb
+++ environments.rb
@@ -1,15 +1,29 @@
 require 'active_record'
 require 'active_record_tasks'
+require 'sinatra'
 
-ActiveRecordTasks.configure do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/TreeOfLife_development')
+# ActiveRecordTasks.configure do
+#   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/TreeOfLife_development')
+
+#   ActiveRecord::Base.establish_connection(
+#     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#     :host     => db.host,
+#     :username => db.user,
+#     :password => db.password,
+#     :database => db.path[1..-1],
+#     :encoding => 'utf8'
+#   )
+# end
+
+configure :production, :development do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
   ActiveRecord::Base.establish_connection(
-    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
   )
 end
