1808dd2add895066bf6a99628e66bf434be08b84
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-07-08 12:44:17 -0500

    Edit environments.rb file.

--- environments.rb
+++ environments.rb
@@ -1,29 +1,17 @@
 require 'active_record'
 require 'active_record_tasks'
-require 'sinatra'
 
-# ActiveRecordTasks.configure do
-#   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/TreeOfLife_development')
+set :environment, :production, :development, :test
 
-#   ActiveRecord::Base.establish_connection(
-#     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-#     :host     => db.host,
-#     :username => db.user,
-#     :password => db.password,
-#     :database => db.path[1..-1],
-#     :encoding => 'utf8'
-#   )
-# end
-
-configure :production do
+ActiveRecordTasks.configure do
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/TreeOfLife_development')
 
   ActiveRecord::Base.establish_connection(
-      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-      :host     => db.host,
-      :username => db.user,
-      :password => db.password,
-      :database => db.path[1..-1],
-      :encoding => 'utf8'
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
   )
 end
