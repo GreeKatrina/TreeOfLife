967ab7476c0db5121457e183ed1a667ef29a66a7
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-07-08 13:30:29 -0500

    Edit environments file for heroku.

--- environments.rb
+++ environments.rb
@@ -3,7 +3,7 @@ require 'active_record_tasks'
 require 'sinatra'
 
 config_path = File.expand_path('../db/config.yml', __FILE__)
-config = YAML.load_file(config_path)
+config = YAML.load ERB.new(File.read config_path).result
 env = ENV['RACK_ENV'] || 'development'
 
 ActiveRecord::Base.establish_connection(config[env])
